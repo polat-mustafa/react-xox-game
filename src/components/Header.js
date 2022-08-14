382d285a8e5ee25ce19b97d9d5b1de2ff258ff60
Mustafa Polat <70276265+polat-mustafa@users.noreply.github.com>
2022-08-14 23:43:41 +0300

    added style to player inputs

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,22 @@ const Header = () => {
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
         autoComplete="off"
+        style={{
+          width: "400px",
+          marginTop: "20px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          padding: "20px",
+          borderRadius: "10px",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+          border: "1px solid #e8e8e8",
+          margin: "0 auto",
+          marginBottom: "20px",
+          textAlign: "center",
+          alignContent: "center",
+        }}
       >
         <Form.Item
           label="Player 1"
@@ -43,6 +59,10 @@ const Header = () => {
               message: "Please input your name!",
             },
           ]}
+          style={{
+            marginBottom: "10px",
+            marginTop: "10px",
+          }}
         >
           <Input />
         </Form.Item>
@@ -55,6 +75,8 @@ const Header = () => {
               message: "Please input your name!",
             },
           ]}
+          style={{ marginTop: "10px" }}
+
         >
           <Input />
         </Form.Item>
